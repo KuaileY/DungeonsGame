34811d03ca1c7471bf563c2ed0acc8d7ed056f55
kuaile_yzh <97369680@qq.com>
2016-10-08 11:10:53 +0800

    Add Views

---
+++ DungeonsGame/Assets/scripts/controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using Entitas;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Systems _systems;
+	void Start ()
+	{
+	    var pools = Pools.sharedInstance;
+	    pools.SetAllPools();
+
+	    _systems = createSystems(pools);
+	    _systems.Initialize();
+	}
+	
+	void Update ()
+	{
+	    _systems.Execute();
+	    _systems.Cleanup();
+	}
+
+    void OnDestroy()
+    {
+        _systems.TearDown();
+    }
+
+    Systems createSystems(Pools pools)
+    {
+        return new Feature("Systems")
+            //input
+
+            ;
+    }
+}
